8c84e62e68d11671015cd3e97808e505e033dc61
evgen <evgenprogrammer@gmail.com>
2023-03-22 13:43:56 +0300

    add map

--- ConsoleCalculator/Calculator.h
+++ ConsoleCalculator/Calculator.h
@@ -1,5 +1,6 @@
 #pragma once
-
+#include <vector>
+#include <map>
 
 
 class Calculator
@@ -8,6 +9,8 @@ private:
 	double n1 ;
 	double n2 ;
 	double ret;
+	std::map<std::string, int> vars;
+	std::map<std::string, int> funcs;
 public:
 	Calculator(double id1, double id2):n1{id1},n2{id2}
 	{
